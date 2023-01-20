99131d872fd21e84eca7b43db5ffa46d21d4b808
Jeremy Gordon <jerrgord@ut.utm.edu>
2023-01-19 20:01:26 -0600

    Fixed break and added last catches took out sleep thread.

--- Git test/Program.cs
+++ Git test/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//Jeremy Gordon
+//1/18/2023
+//CW2 getting into git
+//Classwork to practice using git pushing and pulling repositories.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,36 +15,50 @@ namespace Git_test
     {
         static void Main(string[] args)
         {
-            Console.Write("Input the First Number: ");
+                       Console.Write("Input the First Number: ");
             int StartingNumb = Convert.ToInt32(Console.ReadLine());
             Console.Write("Input the Second Number: ");
             int EndNumber = Convert.ToInt32(Console.ReadLine());
 
-            int RndmNUM = new Random().Next(StartingNumb, EndNumber);
-            int NmbOfAttempts = 0;
-            Console.WriteLine("Please enter a number from " + StartingNumb + " to " + EndNumber);
-
-            while (true)
+            while (StartingNumb >= EndNumber - 1)
             {
-                int guess = GetGuess();
-                NmbOfAttempts++;
-                if (guess == RndmNUM)
-                {
-                    Console.WriteLine($"Number of Attempts: {NmbOfAttempts}");
-                    Console.WriteLine("Your guess is Right!");
-                    System.Threading.Thread.Sleep(2300);
-                    break;
+                Console.Write("Please select another starting number. The starting Number can't be greater than or equal to the ending number - 1: ");
+                StartingNumb = Convert.ToInt32(Console.ReadLine());
+                Console.Write("The ending Number can't be less than the starting number: ");
+                EndNumber = Convert.ToInt32(Console.ReadLine());
+            }
 
-                }
+            int RndmNUM = new Random().Next(StartingNumb + 1, EndNumber - 1);
+            int NmbOfAttempts = 1;
+            Console.WriteLine("Please enter a number from " + StartingNumb + " to " + EndNumber);
+            int guess = GetGuess();
+            string choice = "";
+
+
+            while (guess != RndmNUM)
+            {
+                NmbOfAttempts++;
+
                 if (guess > RndmNUM)
                 {
                     Console.WriteLine("Your guess is too high");
-                }
-                if (guess < RndmNUM)
-                {
-                    Console.WriteLine("Your  guess is too low");
+                    Console.WriteLine("Please enter a number from " + StartingNumb + " to " + EndNumber);
+                    guess = GetGuess();
                 }
 
+                if (guess < RndmNUM)
+                {
+                    Console.WriteLine("Your guess is too low");
+                    Console.WriteLine("Please enter a number from " + StartingNumb + " to " + EndNumber);
+                    guess = GetGuess();
+                }
+            }
+            Console.WriteLine($"Number of Attempts: {NmbOfAttempts}");
+            Console.WriteLine("Your guess is Right!");
+            while (choice == "")
+            {
+                Console.WriteLine("Enter any key to exit");
+                choice = Convert.ToString(Console.ReadLine());
             }
         }
 
@@ -47,17 +66,8 @@ namespace Git_test
         private static int GetGuess()
         {
             int guess = 0;
-            try
-            {
-                guess = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Please Enter a whole number between 1 and 100");
-                GetGuess();
-            }
+            guess = Convert.ToInt32(Console.ReadLine());
             return guess;
-
         }
     }
 }
